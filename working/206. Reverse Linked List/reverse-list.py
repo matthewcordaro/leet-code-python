b707e866fd5a674bd8c91196fb335c79d6061715
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-24 17:13:44 -0400

    removed list from boilerplate and added missing whitespace

--- working/206. Reverse Linked List/reverse-list.py
+++ working/206. Reverse Linked List/reverse-list.py
@@ -1,11 +1,24 @@
 import unittest
-from typing import List
+from typing import List, Optional
 
 
-# O()
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# O(n)
+# There is probably better way with memory updating the pointers backwards
+# But this is best for computation.
 class Solution:
-  # FUNCTION HERE
-  pass
+  def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    r: Optional[ListNode] = None
+    while head is not None:
+      r = ListNode(head.val, r)
+      head = head.next
+    return r
 
 
 class TestSolution(unittest.TestCase):
@@ -13,7 +26,6 @@ class TestSolution(unittest.TestCase):
     self.sol = Solution()
 
   def test_solution(self):
-    self.assertEqual(1, 1)
     pass
 
 
