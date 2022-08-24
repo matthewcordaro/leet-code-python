07fcf9c2a53c4adfcceca69d2c25329bab112757
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-24 19:29:22 -0400

    21 working & done

--- working/21. Merge Two Sorted Lists/merge-sorted-list.py
+++ working/21. Merge Two Sorted Lists/merge-sorted-list.py
@@ -4,36 +4,35 @@ from typing import Optional
 
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
 
 
 # O(n)
 class Solution:
   def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    # Base cases
-    if list1 is None:
-      return list2
-    if list2 is None:
-      return list1
+    # returner + a pointer for help
+    ret = p = ListNode()
 
-    # both should have something set return val to the smallest head
-    ret = list1
-    if list1.val > list2.val:
-      ret = list2
-
-    while list1 and list2 is not None:
+    # merge while 2 lists exist
+    while list1 and list2:
       if list1.val <= list2.val:
-        temp = list1.next
-        list1.next = list2
-        list1 = temp
+        p.next = list1
+        list1 = list1.next
+        p = p.next
       else:
-        temp = list2.next
-        list2.next = list1
-        list2 = temp
+        p.next = list2
+        list2 = list2.next
+        p = p.next
 
-    return ret
+    # add the end of remaining if it exists
+    if list1:
+      p.next = list1
+    elif list2:
+      p.next = list2
+
+    return ret.next
 
 
 class TestSolution(unittest.TestCase):
@@ -45,7 +44,7 @@ class TestSolution(unittest.TestCase):
 
 
 def main():
-    unittest.main()
+  unittest.main()
 
 
 if __name__ == '__main__':
