2e11f9d3527e8801367ccf31b2cf8f7f677aa37e
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-24 13:18:09 -0400

    add boilerplate

---
+++ todo/21. Merge Two Sorted Lists/merge-sorted-list.py
@@ -0,0 +1,24 @@
+import unittest
+from typing import List
+
+
+# O()
+class Solution:
+  # FUNCTION HERE
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertEqual(1,1)
+    pass
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+  main()
