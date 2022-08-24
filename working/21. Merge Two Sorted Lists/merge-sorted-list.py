5c1b17e523619dce5705f61cc472f03b0ec288b5
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-24 17:22:25 -0400

    21. prepped

--- working/21. Merge Two Sorted Lists/merge-sorted-list.py
+++ working/21. Merge Two Sorted Lists/merge-sorted-list.py
@@ -1,11 +1,18 @@
 import unittest
-from typing import List
+from typing import Optional
 
 
-# O()
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+#O(n)
 class Solution:
-  # FUNCTION HERE
-  pass
+  def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    pass
 
 
 class TestSolution(unittest.TestCase):
