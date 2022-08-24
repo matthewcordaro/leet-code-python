6b243da5f7985d031eea68748f1dce44558dc99d
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-24 18:56:24 -0400

    21 not working

--- working/21. Merge Two Sorted Lists/merge-sorted-list.py
+++ working/21. Merge Two Sorted Lists/merge-sorted-list.py
@@ -9,10 +9,31 @@ class ListNode:
         self.next = next
 
 
-#O(n)
+# O(n)
 class Solution:
   def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    pass
+    # Base cases
+    if list1 is None:
+      return list2
+    if list2 is None:
+      return list1
+
+    # both should have something set return val to the smallest head
+    ret = list1
+    if list1.val > list2.val:
+      ret = list2
+
+    while list1 and list2 is not None:
+      if list1.val <= list2.val:
+        temp = list1.next
+        list1.next = list2
+        list1 = temp
+      else:
+        temp = list2.next
+        list2.next = list1
+        list2 = temp
+
+    return ret
 
 
 class TestSolution(unittest.TestCase):
@@ -21,7 +42,6 @@ class TestSolution(unittest.TestCase):
 
   def test_solution(self):
     self.assertEqual(1, 1)
-    pass
 
 
 def main():
