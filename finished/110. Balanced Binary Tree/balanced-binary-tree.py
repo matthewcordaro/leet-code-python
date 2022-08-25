e04742a235191badaf1707b538304dc249cfcd02
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-25 15:59:17 -0400

    110 finished

---
+++ finished/110. Balanced Binary Tree/balanced-binary-tree.py
@@ -0,0 +1,49 @@
+import unittest
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+# O(n)
+class Solution:
+  def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    # Compare each side height at each node with dfs
+    # -1 means unbalanced
+    def dfs(r: Optional[TreeNode]) -> (int):
+      # None case - balanced
+      if r is None: return 0
+
+      # Get depth
+      ld = dfs(r.left)
+      rd = dfs(r.right)
+
+      # check valid depth
+      if ld == -1 or rd == -1 or abs(ld - rd) > 1: return -1
+
+      # valid, add height
+      return 1 + max(ld, rd)
+
+    # any number is balanced here
+    return dfs(root) != -1
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertEqual(1, 1)
+
+
+def main():
+  unittest.main()
+
+
+if __name__ == '__main__':
+  main()
