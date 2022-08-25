e2bb92d7c08066665ba0cbd37b44587fa229e69a
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-24 21:57:14 -0400

    226 Got to Solution very quick :)

---
+++ working/226. Invert Binary Tree/invert-binary-tree.py
@@ -0,0 +1,39 @@
+import unittest
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# O()
+class Solution:
+  def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if root:
+      s_left = s_right = None
+      if root.left:
+        s_right = self.invertTree(root.left)
+      if root.right:
+        s_left = self.invertTree(root.right)
+      root.left, root.right = s_left, s_right
+    return root
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertEqual(1, 1)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+  main()
