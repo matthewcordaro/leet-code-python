0c079a84394c8d4973005ff7786c3263993b2216
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-08-25 17:07:51 -0400

    100 Solution cleaned.

--- finished/100. Same Tree/same-tree.py
+++ finished/100. Same Tree/same-tree.py
@@ -13,13 +13,9 @@ class TreeNode:
 # O(n)
 class Solution:
   def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    if p and q:
-      return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-    elif not p and not q:
-      return True
-    else:
-      return False
-
+    if not p and not q: return True  # both None
+    elif not (p and q): return False  # Not both defined
+    return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
 
 class TestSolution(unittest.TestCase):
