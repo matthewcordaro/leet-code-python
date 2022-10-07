4b5709845c49900b049be5a14c0f5d0e888e52d7
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-10-07 15:01:23 -0400

    added problems

---
+++ working/1851. Minimum Interval to Include Each Query/minimum-interval-to-include-n.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+# O()
+class Solution:
+  def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+    pass
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertEqual(1, 1)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+  main()
