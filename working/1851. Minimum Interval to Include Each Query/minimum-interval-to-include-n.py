e924eb02f41588745bdff94031d150c62b5c78b3
Matthew Cordaro <cordaro.matthew@gmail.com>
2022-10-07 16:02:05 -0400

    Partial coding of min intervals

--- working/1851. Minimum Interval to Include Each Query/minimum-interval-to-include-n.py
+++ working/1851. Minimum Interval to Include Each Query/minimum-interval-to-include-n.py
@@ -1,10 +1,30 @@
 import unittest
+from cmath import inf
+from typing import List
 
 
 # O()
 class Solution:
   def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-    pass
+    # calc interval size by subtracting: right - left + 1
+    size_of_interval = []
+    for (left, right) in intervals:
+      size_of_interval.append(right - left + 1)
+
+    a_very_large_number = 10 ** 10
+    solution = []
+    for idq, query in enumerate(queries):
+      minimum = a_very_large_number
+      for idi, (left, right) in enumerate(intervals):
+        # subtract each query from each integer in the interval
+        # if 0 between them it's a possible solution
+        if left - query <= 0 <= right - query:
+          minimum = min(minimum, size_of_interval[idi])
+        if minimum is not a_very_large_number:
+          solution[idq] = minimum
+        else:
+          solution[idq] = -1
+    return solution
 
 
 class TestSolution(unittest.TestCase):
@@ -12,11 +32,14 @@ class TestSolution(unittest.TestCase):
     self.sol = Solution()
 
   def test_solution(self):
-    self.assertEqual(1, 1)
+    self.assertEqual(self.sol.minInterval(
+      intervals=[[1, 4], [2, 4], [3, 6], [4, 4]],
+      queries=[2, 3, 4, 5]),
+      [3, 3, 1, 4])
 
 
 def main():
-    unittest.main()
+  unittest.main()
 
 
 if __name__ == '__main__':
