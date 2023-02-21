07d4aa990addc3648ff05a8c1b8427823a1698ba
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-02-21 13:03:10 -0500

    Pascal 2 solved, plus a pruned solution

--- working/119. Pascal's Triangle II/pascal-2-half-row-calculations.py
+++ working/119. Pascal's Triangle II/pascal-2-half-row-calculations.py
@@ -11,12 +11,20 @@ class Solution:
     # Solution not stored? Keep building
     while rowIndex > len(self.pascal_triangle) - 1:
       bottom_row = self.pascal_triangle[-1]
-      new_row = [1]  # Each row begins with 1
-      # Calc & build row's middle
-      for i in range(1, len(bottom_row)):
+      new_row = [1]  # Each row begins with a 1
+
+      # Calculate & finish first half
+      for i in range(1, int(len(bottom_row) / 2) + 1):
         new_row.append(bottom_row[i - 1] + bottom_row[i])
         i += 1
-      new_row.append(1)  # Each row ends with a 1
+
+      # Copy first half to second
+      # Note: Converting a float type to int type removes the decimal part.
+      #   Odd length rows will be `n.5 -> n` ex: 2.5 -> 2
+      #   So here the middle number will be skipped.
+      for i in range(int((len(bottom_row) - 1) / 2), -1, -1):
+        new_row.append(new_row[i])
+
       self.pascal_triangle.append(new_row)
     return self.pascal_triangle[rowIndex]
 
@@ -26,9 +34,15 @@ class TestSolution(unittest.TestCase):
     self.sol = Solution()
 
   def test_solution(self):
+    # Documentation on Python site wrong.
+    # Expected: 1st argument
+    # Actual:   2nd argument (items to test)
     self.assertEqual([1], self.sol.getRow(0))
     self.assertEqual([1, 1], self.sol.getRow(1))
+    self.assertEqual([1, 2, 1], self.sol.getRow(2))
+    self.assertEqual([1, 3, 3, 1], self.sol.getRow(3))
     self.assertEqual([1, 4, 6, 4, 1], self.sol.getRow(4))
+    self.assertEqual([1, 5, 10, 10, 5, 1], self.sol.getRow(5))
     self.assertEqual([1,
                       20,
                       190,
