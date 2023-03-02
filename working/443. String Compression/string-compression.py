7ba0883b189b591d5fe1709e083213f5bf00bde7
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-02 00:39:01 -0500

    Solved 443: Ugly Needs cleanup

---
+++ working/443. String Compression/string-compression.py
@@ -0,0 +1,57 @@
+import unittest
+
+
+# O()
+class Solution:
+  # noinspection PyMethodMayBeStatic
+  def compress(self, chars: list[str]) -> int:
+    count = 1
+    end = 1
+    for i in range(1, len(chars)):
+      if chars[i] == chars[i - 1]:
+        count += 1
+        continue
+      if count > 1:
+        for c in str(count):
+          chars[end] = c
+          end += 1
+        count = 1
+      chars[end] = chars[i]
+      end += 1
+    if count > 1:
+      for c in str(count):
+        chars[end] = c
+        end += 1
+    for c in chars: print(c, end='')
+    print()
+    return end
+
+
+def put_num_in_location(chars, char_count, *last_pointer):
+  pass
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    # Documentation on Python site wrong.
+    # Expected: 1st argument
+    # Actual:   2nd argument (items to test)
+    self.assertEqual(9, self.sol.compress(
+      ["a", "a", "a", "b", "c", "d", "d", "d", "d", "d", "e", "f", "f"]))
+
+    self.assertEqual(8, self.sol.compress(
+      ["a", "a", "a", "b", "c", "d", "d", "d", "d", "d", "e", "f"]))
+
+    self.assertEqual(3, self.sol.compress(
+      ["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]))
+
+
+def main():
+  unittest.main()
+
+
+if __name__ == '__main__':
+  main()
