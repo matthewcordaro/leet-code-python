b5ff5a88a3b8c792acf3809573170ae1067857a3
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-09 22:39:52 -0500

    Solved 167 Two Sum II Forgot to push code from Feb.  Slightly better solution added.

---
+++ finished/167. Two Sum II - Input Array Is Sorted/two-sum.py
@@ -0,0 +1,30 @@
+import unittest
+from typing import List
+
+
+# O(n)
+class Solution:
+  def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    j = len(numbers) - 1
+    for i in range(len(numbers)):
+      # if j < i: return []
+      while numbers[i] + numbers[j] > target:
+        j -= 1
+      if numbers[i] + numbers[j] == target:
+        return [i + 1, j + 1]
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_twoSum(self):
+    self.assertEqual(self.sol.twoSum([1, 2, 3, 4, 5, 6], 4), [1, 3])
+
+
+def main():
+  unittest.main()
+
+
+if __name__ == '__main__':
+  main()
