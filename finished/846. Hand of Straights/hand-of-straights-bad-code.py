d8e31219f3bd54ee72a1e93f2e368e4f8f92f914
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-09 22:23:40 -0500

    Solved 846 Hand of Straights

---
+++ finished/846. Hand of Straights/hand-of-straights-bad-code.py
@@ -0,0 +1,44 @@
+import unittest
+from typing import List
+
+
+# O()
+class Solution:
+  def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+    if len(hand) % groupSize != 0: return False
+    hand.sort()
+    groups: [[int]] = []
+    for card in hand:
+      groups = self.putCardIntoGroups(card, groups, groupSize)
+    return all([len(group) == groupSize for group in groups])
+
+  def putCardIntoGroups(self, card: int, groups: [[int]], groupSize: int) -> [[int]]:
+    full_group = None
+    for group in groups:
+      # Check if last card matches
+      if group[-1] == card - 1:
+        if len(group) == groupSize - 1:
+          full_group = group
+          break
+        group.append(card)
+        break
+    if full_group:
+      groups.remove(full_group)
+    return groups
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertTrue(self.sol.isNStraightHand(hand=[1, 2, 3, 6, 2, 3, 4, 7, 8], groupSize=3))
+    self.assertFalse(self.sol.isNStraightHand(hand=[8, 10, 12], groupSize=3))
+
+
+def main():
+  unittest.main()
+
+
+if __name__ == '__main__':
+  main()
