6ed947a9e2e8d4d1391a62531b1c643fb0ff141f
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-14 23:40:17 -0400

    Solved 142

---
+++ finished/142. Linked List Cycle II/linked-list-cycle-2-floyd.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# O(n) - Memory: O(1)  Floyd's Algo
+class Solution:
+  # noinspection PyMethodMayBeStatic
+  def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    turtle = rabbit = head
+    while rabbit and rabbit.next:
+      turtle, rabbit = turtle.next, rabbit.next.next  # Move by 1 and 2 respectively
+      if turtle == rabbit:  # A cycle exists
+        turtle = head  # reset
+        while rabbit != turtle: turtle, rabbit = turtle.next, rabbit.next
+        return turtle
+    return None  # No Cycle
