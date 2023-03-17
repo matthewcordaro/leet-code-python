7ff9231120160a7713403095cc5380f510f78df3
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-16 23:28:42 -0400

    Solved 347

--- working/347. Top K Frequent Elements/top-k-frequent-elements.py
+++ working/347. Top K Frequent Elements/top-k-frequent-elements.py
@@ -1,11 +1,20 @@
+import heapq
 import unittest
+from collections import defaultdict
+from typing import List
 
 
 # O()
 class Solution:
   # noinspection PyMethodMayBeStatic
-  def someFunction(self):
-    pass
+  def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    freq = defaultdict(lambda: 0)  # start frequency counting from 0
+    for num in nums: freq[num] -= 1  # go negative for heapq
+    heap = []
+    for num, quantity in freq.items(): heapq.heappush(heap, (quantity, num))
+    nums = []  # repurpose nums for the solution
+    while (k:=k-1) >= 0: nums.append(heapq.heappop(heap)[1])
+    return nums
 
 
 class TestSolution(unittest.TestCase):
@@ -16,7 +25,8 @@ class TestSolution(unittest.TestCase):
     # Documentation on Python site wrong.
     # Expected: 1st argument
     # Actual:   2nd argument (items to test)
-    self.assertEqual(0, self.sol.someFunction())
+    self.assertEqual([1,2], self.sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2))
+    self.assertEqual([1], self.sol.topKFrequent(nums = [1], k = 1))
 
 
 def main():
