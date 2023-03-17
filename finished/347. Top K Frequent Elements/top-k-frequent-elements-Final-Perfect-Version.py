6cc30b2118bdf7c82f77b60e6126d8b60614e6a8
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-16 23:52:52 -0400

    Perfected 347.  Can't be any better.

---
+++ finished/347. Top K Frequent Elements/top-k-frequent-elements-Final-Perfect-Version.py
@@ -0,0 +1,31 @@
+import heapq
+import unittest
+from collections import Counter
+from typing import List
+
+
+# O()
+class Solution:
+  # noinspection PyMethodMayBeStatic
+  def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    return [num for num, _ in Counter(nums).most_common(k)]
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    # Documentation on Python site wrong.
+    # Expected: 1st argument
+    # Actual:   2nd argument (items to test)
+    self.assertEqual([1,2], self.sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2))
+    self.assertEqual([1], self.sol.topKFrequent(nums = [1], k = 1))
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+  main()
