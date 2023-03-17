5b4b09c6eff36856b6a1334255e10f8c8857b7cb
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-16 23:10:57 -0400

    Start on 347

---
+++ working/347. Top K Frequent Elements/top-k-frequent-elements.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+# O()
+class Solution:
+  # noinspection PyMethodMayBeStatic
+  def someFunction(self):
+    pass
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    # Documentation on Python site wrong.
+    # Expected: 1st argument
+    # Actual:   2nd argument (items to test)
+    self.assertEqual(0, self.sol.someFunction())
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+  main()
