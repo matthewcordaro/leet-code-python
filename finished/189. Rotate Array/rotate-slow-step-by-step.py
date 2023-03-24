c83f6d09f65cbd6676d0a6421869651133a96bf6
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-03-24 19:20:36 -0400

    Finished 189

---
+++ finished/189. Rotate Array/rotate-slow-step-by-step.py
@@ -0,0 +1,30 @@
+import unittest
+from typing import List
+
+
+# O()
+class Solution:
+  # noinspection PyMethodMayBeStatic
+  def rotate(self, nums: List[int], k: int):
+    k %= len(nums)  # avoid extra rotation
+    while (k:=k-1) >= 0:
+      t=nums[-1]
+      for i in range(len(nums)-1, 0, -1):
+        nums[i] = nums[i-1]
+      nums[0] = t
+    return nums
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertEqual([5,6,7,1,2,3,4], self.sol.rotate([1, 2, 3, 4, 5, 6, 7], 3))
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+  main()
