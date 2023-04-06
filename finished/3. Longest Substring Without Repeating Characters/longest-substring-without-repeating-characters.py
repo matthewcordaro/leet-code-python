58e6d46b67a30c898db4ba005432f39432400bdd
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-04-05 23:55:28 -0400

    Solved 3 - Two Pointers

---
+++ finished/3. Longest Substring Without Repeating Characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+
+# O()
+class Solution:
+  # noinspection PyMethodMayBeStatic
+  def lengthOfLongestSubstring(self, s: str) -> int:
+    mx = 0
+    cs = set()
+    l = r = 0
+    while r < len(s):
+      if s[r] in cs:  # move left & remove
+        cs.remove(s[l])
+        l +=1
+      else:  # move right & add
+        cs.add(s[r])
+        mx = max(len(cs), mx)
+        r += 1
+    return mx
+
+
+class TestSolution(TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertEqual(3, self.sol.lengthOfLongestSubstring("abcabcbb"))
+    self.assertEqual(1, self.sol.lengthOfLongestSubstring("bbbbb"))
+    self.assertEqual(3, self.sol.lengthOfLongestSubstring("pwwkew"))
+    self.assertEqual(2, self.sol.lengthOfLongestSubstring("aab"))
+    self.assertEqual(3, self.sol.lengthOfLongestSubstring("dvdf"))
+
+
+def main():
+    super(TestSolution())
+
+
+if __name__ == '__main__':
+  main()
