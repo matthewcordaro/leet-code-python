d919ed4e7423cdbe734d924b8fd5eb3654f58ae9
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-05-12 00:12:15 -0400

    Cleaned up 100

--- finished/100. Same Tree/same-tree.py
+++ finished/100. Same Tree/same-tree.py
@@ -4,18 +4,20 @@ from typing import Optional
 
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
 
 
 # O(n)
 class Solution:
   def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    if not p and not q: return True  # both None
-    elif not (p and q): return False  # Not both defined
-    return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    if not (p or q): return True  # Both are None
+    if not (p and q): return False  # One is None
+    return p.val == q.val \
+      and self.isSameTree(p.left, q.left) \
+      and self.isSameTree(p.right, q.right)
 
 
 class TestSolution(unittest.TestCase):
@@ -27,7 +29,7 @@ class TestSolution(unittest.TestCase):
 
 
 def main():
-    unittest.main()
+  unittest.main()
 
 
 if __name__ == '__main__':
