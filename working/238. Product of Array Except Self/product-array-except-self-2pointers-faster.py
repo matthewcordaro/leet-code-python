b302652973f2e3403e400f144d3a21f2698b2df8
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-07-24 15:50:32 -0400

    Cleared up 238

---
+++ working/238. Product of Array Except Self/product-array-except-self-2pointers-faster.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+
+# O()
+class Solution:
+  # noinspection PyMethodMayBeStatic
+  def productExceptSelf(self, nums: list[int]) -> list[int]:
+    l = len(nums)
+    sol = [1] * l
+    pre = post = 1
+    # 2 ptrs: Update Sol, then update `pre` & `post`,
+    for i in range(l):
+      j = l-i-1
+      sol[i] *= pre
+      sol[j] *= post
+      pre *= nums[i]
+      post *= nums[j]
+
+    return sol
+
+
+class TestSolution(TestCase):
+  def setUp(self):
+    self.sol = Solution()
+
+  def test_solution(self):
+    self.assertEqual([24,12,8,6], self.sol.productExceptSelf([1,2,3,4]))
+    self.assertEqual([0,0,9,0,0], self.sol.productExceptSelf([-1,1,0,-3,3]))
+
+
+def main():
+    super(TestSolution())
+
+
+if __name__ == '__main__':
+  main()
