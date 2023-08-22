d550c7d0d2352e23dfb80fb4bf81ecd83be5305a
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-08-21 22:56:12 -0400

    Cleanup

--- needs-speed/78. Subsets/subsets - backtracking.py
+++ needs-speed/78. Subsets/subsets - backtracking.py
@@ -3,48 +3,48 @@ from unittest import TestCase
 
 # O()
 class Solution:
-  # noinspection PyMethodMayBeStatic
-  # Choices:  Either take then number or not.
-  # Constraints:  None really; only take 1 or none
-  # Goal: deciding on each number
-  # All really the same thing.
-  #
-  def subsets(self, nums: list[int]) -> list[list[int]]:
-    solution = []
-    subset_from_path = []
+    # noinspection PyMethodMayBeStatic
+    # Choices:  Either take then number or not.
+    # Constraints:  None really; only take 1 or none
+    # Goal: deciding on each number
+    # All really the same thing.
+    #
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        solution = []
+        subset_from_path = []
 
-    def depth_first_search(index):
-      # Base Case Out Of Bounds
-      if index >= len(nums):
-        # Since we're at the end, we've reached a goal!
-        # Save a copy of the current working subset in the solution set
-        solution.append(subset_from_path.copy())
-        return
+        def depth_first_search(index):
+            # Base Case Out Of Bounds
+            if index >= len(nums):
+                # Since we're at the end, we've reached a goal!
+                # Save a copy of the current working subset in the solution set
+                solution.append(subset_from_path.copy())
+                return
 
-      # EXPLORE: Lets take the number (at index)...
-      subset_from_path.append(nums[index])
-      # And continue on
-      depth_first_search(index + 1)
+            # EXPLORE: Lets take the number (at index)...
+            subset_from_path.append(nums[index])
+            # And continue on
+            depth_first_search(index + 1)
 
-      # BACKTRACK: Remove the number we took, so we can try another path.
-      subset_from_path.pop()
+            # BACKTRACK: Remove the number we took, so we can try another path.
+            subset_from_path.pop()
 
-      # EXPLORE: Lets skip to the number...
-      # And continue on
-      depth_first_search(index + 1)
+            # EXPLORE: Lets skip to the number...
+            # And continue on
+            depth_first_search(index + 1)
 
-    # execute the search from the root
-    depth_first_search(0)
-    return solution
+        # execute the search from the root
+        depth_first_search(0)
+        return solution
 
 
 class TestSolution(TestCase):
-  def setUp(self):
-    self.sol = Solution()
+    def setUp(self):
+        self.sol = Solution()
 
-  def test_solution(self):
-    ans = [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-    self.assertListEqual(sorted(ans), sorted(self.sol.subsets(nums = [1,2,3])))
+    def test_solution(self):
+        ans = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+        self.assertListEqual(sorted(ans), sorted(self.sol.subsets(nums=[1, 2, 3])))
 
 
 def main():
@@ -52,4 +52,4 @@ def main():
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- needs-speed/78. Subsets/subsets - recursive.py
+++ needs-speed/78. Subsets/subsets - recursive.py
@@ -3,26 +3,28 @@ from unittest import TestCase
 
 # O()
 class Solution:
-  # noinspection PyMethodMayBeStatic
-  def subsets(self, nums: list[int]) -> list[list[int]]:
-    if len(nums) == 0: return [[]]
-    solution = []
-    for i, n in enumerate(nums):
-      # calculate all solutions further down the tree
-      further_solutions = self.subsets(nums[i+1:])
-      # Now extend back with the including and excluding n:
-      solution.extend(further_solutions)
-      for fs in further_solutions:
-        solution.append(fs.append(n))
-    return solution
+    # noinspection pymethodmaybestatic
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        if len(nums) == 0:
+            return [[]]
+        solution = []
+        for i, n in enumerate(nums):
+            # calculate all solutions further down the tree
+            further_solutions = self.subsets(nums[i + 1:])
+            # Now extend back with the including and excluding n:
+            solution.extend(further_solutions)
+            for fs in further_solutions:
+                solution.append(fs.append(n))
+        return solution
+
 
 class TestSolution(TestCase):
-  def setUp(self):
-    self.sol = Solution()
+    def setUp(self):
+        self.sol = Solution()
 
-  def test_solution(self):
-    ans = [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-    self.assertEqual(ans, self.sol.subsets(nums = [1,2,3]))
+    def test_solution(self):
+        ans = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+        self.assertEqual(ans, self.sol.subsets(nums=[1, 2, 3]))
 
 
 def main():
@@ -30,4 +32,4 @@ def main():
 
 
 if __name__ == '__main__':
-  main()
+    main()
