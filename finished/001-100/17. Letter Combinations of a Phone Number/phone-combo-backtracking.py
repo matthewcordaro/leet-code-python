e2c822a51cf63fe0db4b215594d87cd855fb2ee3
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-08-21 22:56:23 -0400

    Cleanup

---
+++ finished/001-100/17. Letter Combinations of a Phone Number/phone-combo-backtracking.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+
+
+# O()
+class Solution:
+    # noinspection PyMethodMayBeStatic
+    def letterCombinations(self, digits: str) -> list[str]:
+        if len(digits) == 0:
+            return []  # Blank Case
+
+        t9_dict = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
+        solution = []
+
+        def depth_first_search(working_solution: str, digit_index: int):
+            # Base Case:  End of solution tree
+            if digit_index == len(digits):
+                solution.append(working_solution)
+                return  # Backtrack
+
+            # Explore:  Try adding each letter to the working solution
+            digit = digits[digit_index]
+            for c in t9_dict[digit]:
+                depth_first_search(working_solution + c, digit_index + 1)
+                # Backtrack:  No need to undo variables, not modified
+
+        depth_first_search("", 0)
+        return solution
+
+
+class TestSolution(TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_solution(self):
+        ans = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+        test = self.sol.letterCombinations("23")
+        self.assertListEqual(sorted(ans), sorted(test))
+        self.assertEqual([], sorted(self.sol.letterCombinations("")))
+
+
+def main():
+    super(TestSolution())
+
+
+if __name__ == '__main__':
+    main()
