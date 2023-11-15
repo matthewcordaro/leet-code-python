533e80b84ec64399c884bb4d17987ce8e867b78e
Matthew Cordaro <cordaro.matthew@gmail.com>
2023-11-14 23:20:33 -0500

    Finished 1846

--- finished/501+/1846. Maximum Element After Decreasing and Rearranging/maximum-element-after-decreasing-and-rearranging.py
+++ finished/501+/1846. Maximum Element After Decreasing and Rearranging/maximum-element-after-decreasing-and-rearranging.py
@@ -4,8 +4,13 @@ from unittest import TestCase
 # O()
 class Solution:
     # noinspection PyMethodMayBeStatic
-    def someFunction(self):
-        pass
+    def maximumElementAfterDecrementingAndRearranging(self, arr: list[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i - 1] > 1:
+                arr[i] = arr[i - 1] + 1
+        return arr[-1]
 
 
 class TestSolution(TestCase):
@@ -13,7 +18,10 @@ class TestSolution(TestCase):
         self.sol = Solution()
 
     def test_solution(self):
-        self.assertEqual(0, self.sol.someFunction())
+        self.assertEqual(2, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[2, 2, 1, 2, 1]))
+        self.assertEqual(3, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[100, 1, 1000]))
+        self.assertEqual(5, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[1, 2, 3, 4, 5]))
+        self.assertEqual(3, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[73, 98, 9]))
 
 
 def main():
--- leet-boilerplate.py
+++ leet-boilerplate.py
@@ -4,13 +4,8 @@ from unittest import TestCase
 # O()
 class Solution:
     # noinspection PyMethodMayBeStatic
-    def maximumElementAfterDecrementingAndRearranging(self, arr: list[int]) -> int:
-        arr.sort()
-        arr[0] = 1
-        for i in range(1, len(arr)):
-            if arr[i] - arr[i - 1] > 1:
-                arr[i] = arr[i - 1] + 1
-        return arr[-1]
+    def someFunction(self):
+        pass
 
 
 class TestSolution(TestCase):
@@ -18,10 +13,7 @@ class TestSolution(TestCase):
         self.sol = Solution()
 
     def test_solution(self):
-        self.assertEqual(2, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[2, 2, 1, 2, 1]))
-        self.assertEqual(3, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[100, 1, 1000]))
-        self.assertEqual(5, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[1, 2, 3, 4, 5]))
-        self.assertEqual(3, self.sol.maximumElementAfterDecrementingAndRearranging(arr=[73, 98, 9]))
+        self.assertEqual(0, self.sol.someFunction())
 
 
 def main():
