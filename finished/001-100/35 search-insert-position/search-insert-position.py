e628c37fbad2cbf06afcab7b5d6cdbe4ce846143
Matthew Cordaro <cordaro.matthew@gmail.com>
2024-09-05 16:29:30 -0400

    35 Theta Improvement The code technically works without the correct calculation of mid because mid trends in the correct direction every time. This fix lets the target actually be found; the error caused the full log(n) search everytime. Bug: Θ(log(n)) Fix: Θ(log(n)/2)

--- finished/001-100/35 search-insert-position/search-insert-position.py
+++ finished/001-100/35 search-insert-position/search-insert-position.py
@@ -8,11 +8,11 @@ class Solution:
 
         def binary_search(low, high):
             if low > high: return low  # Not in list
-            mid = high + low // 2
+            mid = (high + low) // 2
             if target == nums[mid]: return mid
             return binary_search(*(low, mid - 1) if target < nums[mid] else (mid + 1, high))
 
-        return binary_search(0, len(nums)-1)
+        return binary_search(0, len(nums) - 1)
 
 
 class TestSolution(TestCase):
@@ -20,10 +20,15 @@ class TestSolution(TestCase):
         self.sol = Solution()
 
     def test_solution(self):
-        self.assertEqual(2, self.sol.searchInsert(nums = [1,3,5,6], target = 5))
-        self.assertEqual(1, self.sol.searchInsert(nums = [1,3,5,6], target = 2))
-        self.assertEqual(4, self.sol.searchInsert(nums = [1,3,5,6], target = 7))
-        self.assertEqual(0, self.sol.searchInsert(nums = [1,3,5,6], target = 0))
+        self.assertEqual(2, self.sol.searchInsert(nums=[1, 3, 5, 6], target=5))
+        self.assertEqual(1, self.sol.searchInsert(nums=[1, 3, 5, 6], target=2))
+        self.assertEqual(4, self.sol.searchInsert(nums=[1, 3, 5, 6], target=7))
+        self.assertEqual(0, self.sol.searchInsert(nums=[1, 3, 5, 6], target=0))
+        a = [1,3,5,7,9,11,13,15,17,19]
+        for n in range(10):
+            self.assertEqual(n, self.sol.searchInsert(nums=a, target=2*n))
+            self.assertEqual(n, self.sol.searchInsert(nums=a, target=2*n+1))
+
 
 
 def main():
