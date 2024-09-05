4fc390bac7e4cafbd4434ed04f7ec08c2c8461d1
Matthew Cordaro <cordaro.matthew@gmail.com>
2024-09-05 17:17:28 -0400

    12 done

---
+++ finished/001-100/12 integer-to-roman/integer-to-roman.py
@@ -0,0 +1,35 @@
+from audioop import reverse
+from unittest import TestCase
+
+
+# O()
+class Solution:
+    # noinspection PyMethodMayBeStatic
+    def intToRoman(self, num: int) -> str:
+        roman_digits = (("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"),
+                        ("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"),
+                        ("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"), ("", "M", "MM", "MMM"))
+        solution = ""
+        digits = [int(x) for x in reversed(str(num))]
+        for digit, i in enumerate(digits):
+            solution = roman_digits[digit][i] + solution
+        return solution
+
+
+class TestSolution(TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_solution(self):
+        self.assertEqual("MMMDCCXLIX", self.sol.intToRoman(3749))
+        self.assertEqual("LVIII", self.sol.intToRoman(58))
+        self.assertEqual("MCMXCIV", self.sol.intToRoman(1994))
+        self.assertEqual("XII", self.sol.intToRoman(13))
+
+
+def main():
+    super(TestSolution())
+
+
+if __name__ == '__main__':
+    main()
