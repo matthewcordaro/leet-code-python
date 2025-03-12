6057ebbfcb9c95eec58fc895c3f9203497df974c
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-03-11 20:57:10 -0400

    Add helper for creating binary trees from level-order list
    Introduce a `create_tree` function to build a binary tree using a list of values in level-order format. This utility simplifies tree creation for testing or other use cases and supports `None` values for empty nodes. Added a supporting `TreeNode` class for node representation.

---
+++ tree_node_helpers.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def create_tree(vals: List[Optional[int]]) -> Optional[TreeNode]:
+    """
+    Helper function to create a binary tree from a list of values in level-order.
+    Args:
+        vals (List[Optional[int]]): A list of values in level-order where None represents an empty node.
+
+    Returns:
+        TreeNode: The root of the binary search tree created.
+    """
+    if not vals:
+        return None
+    nodes = [TreeNode(v) if v is not None else None for v in vals]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids:
+                node.left = kids.pop()
+            if kids:
+                node.right = kids.pop()
+    return root
